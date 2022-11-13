234ad3e8eaa80da68f47321d9947e36b47a17ef5
Rob <rob@example.com>
2022-11-13 18:08:13 +0300

    mobile

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import logo from '../assets/Vector.svg'
+import circeya from '../assets/Circeya.svg'
+import phone from '../assets/Phone.svg'
 
 const Navigation = () => {
   return (
     <nav className="nav">
       <div className="nav-inner">
         <div className="logo">
-          <img src={logo} alt="" />
-          <h2>CIRCEYA</h2>
+          <img className="logo-img" src={logo} alt="logo" />
+          <img className="logo-title" src={circeya} alt="circeya" />
+
+          {/* <h2>CIRCEYA</h2> */}
         </div>
 
-        <div>
-          <p>+7 (495) 495-49-54</p>
+        <div className="phone">
+          <p className="phone-full">+7 (495) 495-49-54</p>
+          <img className="phone-short" src={phone} alt="phone" />
         </div>
       </div>
     </nav>
